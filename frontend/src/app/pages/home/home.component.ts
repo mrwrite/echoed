2e34d8bccc236bcca3605cbd5c32ecc03ebdf0ed
Anthony Wright <anthony@mrwrite.dev>
2025-07-29 12:18:21 -0500

    Add lesson fullscreen mode and adjust media components

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,31 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../../components/echo-sidebar/echo-sidebar.component';
 import { EchoHeaderComponent } from '../../components/echo-header/echo-header.component';
 import { UserDashboardComponent } from "../user-dashboard/user-dashboard.component";
 import { UserInfo } from '../../models/user-info';
 import { AuthService } from '../../services/auth.service';
+import { trigger, style, animate, transition } from '@angular/animations';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, SidebarComponent, EchoHeaderComponent,RouterOutlet],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-out', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   sidebarCollapsed = false;
   userInfo!: UserInfo;
+  lessonMode = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     var access_token = localStorage.getItem('token');
     if (access_token) {
-      this.userInfo = this.authService.getTokenPayload(access_token);      
+      this.userInfo = this.authService.getTokenPayload(access_token);
       console.log(this.userInfo);
     }
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.lessonMode = this.router.url.includes('/lesson/');
+      });
   }
 
 }
